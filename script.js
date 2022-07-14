37c3c597616529cc22d2de5fd0f1870a19502dba
Mary Alex <68873062+Ligzandra@users.noreply.github.com>
2022-07-14 19:34:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var twoSum = function(nums, target) {
+    for(var i=0;i<nums.length;i++){
+        for(var x=i+1;x<nums.length;x++){
+        checkSum = nums[i]+nums[x];
+        if(checkSum == target){
+            return [nums[i],nums[x]]
+        }
+      }
+    }
+    
+};
+
+
+console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([3, 2, 4],5))
+console.log(twoSum([3,6,4,2],6))
